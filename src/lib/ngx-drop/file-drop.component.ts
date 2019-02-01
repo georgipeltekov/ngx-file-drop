2d1e31258335c7197c3969a77de41dc4bc412ece
Christoph Dörfel <christoph.doerfel@gmail.com>
2019-02-01 10:51:05 +0100

    Fix missing event reference causing exception in Firefox

--- src/lib/ngx-drop/file-drop.component.ts
+++ src/lib/ngx-drop/file-drop.component.ts
@@ -1,4 +1,15 @@
-import { Component, Input, Output, EventEmitter, NgZone, OnDestroy, Renderer } from '@angular/core';
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  NgZone,
+  OnDestroy,
+  Renderer,
+  ViewChild,
+  ViewContainerRef,
+  ElementRef
+} from '@angular/core';
 import { timer, Subscription } from 'rxjs';
 
 import { UploadFile } from './upload-file.model';
@@ -10,8 +21,6 @@ import { FileSystemFileEntry, FileSystemEntryMetadata, FileSystemEntry, FileSyst
   templateUrl: './file-drop.component.html',
   styleUrls: ['./file-drop.component.scss']
 })
-
-
 export class FileComponent implements OnDestroy {
 
   @Input()
@@ -22,6 +31,8 @@ export class FileComponent implements OnDestroy {
   disableIf: boolean = false;
   @Input()
   showBrowseBtn: boolean = false;
+  @Input()
+  browseBtnLabel: string = 'Browse files';
 
   @Output()
   public onFileDrop: EventEmitter<UploadEvent> = new EventEmitter<UploadEvent>();
@@ -30,19 +41,19 @@ export class FileComponent implements OnDestroy {
   @Output()
   public onFileLeave: EventEmitter<any> = new EventEmitter<any>();
 
+  @ViewChild('fileSelector', {read: ViewContainerRef})
+  public fileSelector: ElementRef;
+
   stack = [];
   files: UploadFile[] = [];
-  subscription: Subscription;
+  subscription: Subscription | null = null;
   dragoverflag: boolean = false;
 
   globalDisable: boolean = false;
   globalStart: Function;
   globalEnd: Function;
 
-  numOfActiveReadEntries = 0
-
-  length;
-  items;
+  numOfActiveReadEntries = 0;
 
   constructor(
     private zone: NgZone,
@@ -60,7 +71,7 @@ export class FileComponent implements OnDestroy {
   }
 
   public onDragOver(event: Event): void {
-    if (!this.globalDisable && !this.disableIf) {
+    if (!this.isDropzoneDisabled()) {
       if (!this.dragoverflag) {
         this.dragoverflag = true;
         this.onFileOver.emit(event);
@@ -70,7 +81,7 @@ export class FileComponent implements OnDestroy {
   }
 
   public onDragLeave(event: Event): void {
-    if (!this.globalDisable && !this.disableIf) {
+    if (!this.isDropzoneDisabled()) {
       if (this.dragoverflag) {
         this.dragoverflag = false;
         this.onFileLeave.emit(event);
@@ -79,75 +90,80 @@ export class FileComponent implements OnDestroy {
     }
   }
 
-  dropFiles(event: any) {
-    this.dragoverflag = false;
-    event.dataTransfer.dropEffect = 'copy';
-    if (event.dataTransfer.items) {
-      this.length = event.dataTransfer.items.length;
-      this.items = event.dataTransfer.items;
-    } else {
-      this.length = event.dataTransfer.files.length;
-      this.items = event.dataTransfer.files;
-    }
-    this.checkFiles();
-  }
-
-  uploadFiles(event: any) {
-    if (event.srcElement) { 
-      this.items = event.srcElement.files;
-      this.length = this.items.length;
-      this.checkFiles();
+  public dropFiles(event: DragEvent): void {
+    if (!this.isDropzoneDisabled()) {
+      this.dragoverflag = false;
+      event.dataTransfer.dropEffect = 'copy';
+      let items: FileList | DataTransferItemList;
+      if (event.dataTransfer.items) {
+        items = event.dataTransfer.items;
+      } else {
+        items = event.dataTransfer.files;
+      }
+      this.preventAndStop(event);
+      this.checkFiles(items);
     }
   }
 
-  checkFiles() {
-    if (!this.globalDisable && !this.disableIf) {
+  public onBrowseButtonClick(event: MouseEvent): void {
+    if (this.fileSelector && this.fileSelector.nativeElement) {
+      (this.fileSelector.nativeElement as HTMLInputElement).click();
+    }
+  }
 
-      for (let i = 0; i < this.length; i++) {
-        let entry: FileSystemEntry;
-        if(this.items[i].webkitGetAsEntry){
-          entry = this.items[i].webkitGetAsEntry();
-        }
-         
-        if (!entry) {
-          const file: File = this.items[i];
-          if (file) {
-            const fakeFileEntry: FileSystemFileEntry = {
-              name: file.name,
-              isDirectory: false,
-              isFile: true,
-              file: (callback: (filea: File) => void): void => {
-                callback(file)
-              }
-            }
-            const toUpload: UploadFile = new UploadFile(fakeFileEntry.name, fakeFileEntry);
-            this.addToQueue(toUpload);
-          }
-        } else {
-          if (entry.isFile) {
-            const toUpload: UploadFile = new UploadFile(entry.name, entry);
-            this.addToQueue(toUpload);
-          } else if (entry.isDirectory) {
-            this.traverseFileTree(entry, entry.name);
-          }
-        }
+  public uploadFiles(event: Event): void {
+    if (!this.isDropzoneDisabled()) {
+      if (event.srcElement) {
+        const items = (event.srcElement as HTMLInputElement).files;
+        this.checkFiles(items);
+      }
+    }
+  }
+
+  private checkFiles(items: FileList | DataTransferItemList): void {
+    for (let i = 0; i < items.length; i++) {
+      let entry: FileSystemEntry;
+      const item = items[i];
+      if (this.canGetAsEntry(item)) {
+        entry = item.webkitGetAsEntry();
       }
 
-      this.preventAndStop(event);
-
-      const timerObservable = timer(200, 200);
-      this.subscription = timerObservable.subscribe(t => {
-        if (this.files.length > 0 && this.numOfActiveReadEntries === 0) {
-          this.onFileDrop.emit(new UploadEvent(this.files));
-          this.files = [];
+      if (!entry) {
+        if (item) {
+          const fakeFileEntry: FileSystemFileEntry = {
+            name: (item as File).name,
+            isDirectory: false,
+            isFile: true,
+            file: (callback: (filea: File) => void): void => {
+              callback(item as File)
+            }
+          };
+          const toUpload: UploadFile = new UploadFile(fakeFileEntry.name, fakeFileEntry);
+          this.addToQueue(toUpload);
         }
-      });
+      } else {
+        if (entry.isFile) {
+          const toUpload: UploadFile = new UploadFile(entry.name, entry);
+          this.addToQueue(toUpload);
+        } else if (entry.isDirectory) {
+          this.traverseFileTree(entry, entry.name);
+        }
+      }
     }
 
+    const timerObservable = timer(200, 200);
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+    this.subscription = timerObservable.subscribe(t => {
+      if (this.files.length > 0 && this.numOfActiveReadEntries === 0) {
+        this.onFileDrop.emit(new UploadEvent(this.files));
+        this.files = [];
+      }
+    });
   }
 
   private traverseFileTree(item: FileSystemEntry, path: string) {
-
     if (item.isFile) {
       const toUpload: UploadFile = new UploadFile(path, item);
       this.files.push(toUpload);
@@ -162,7 +178,7 @@ export class FileComponent implements OnDestroy {
       const thisObj = this;
 
       const readEntries = function () {
-        thisObj.numOfActiveReadEntries++
+        thisObj.numOfActiveReadEntries++;
         dirReader.readEntries(function (res) {
           if (!res.length) {
             // add empty folders
@@ -194,6 +210,14 @@ export class FileComponent implements OnDestroy {
     }
   }
 
+  private canGetAsEntry(item: any): item is DataTransferItem {
+    return !!item.webkitGetAsEntry;
+  }
+
+  private isDropzoneDisabled(): boolean {
+    return (this.globalDisable || this.disableIf);
+  }
+
   private addToQueue(item: UploadFile) {
     this.files.push(item);
   }
@@ -218,6 +242,7 @@ export class FileComponent implements OnDestroy {
   ngOnDestroy() {
     if (this.subscription) {
       this.subscription.unsubscribe();
+      this.subscription = null;
     }
     this.globalStart();
     this.globalEnd();
