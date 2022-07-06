9e3ef0e0aae30c4ed0f85bf7a3471fb8d035e317
Bobby Galli <bobbyg603@gmail.com>
2022-07-06 09:46:24 -0400

    chore: update sample

--- projects/ngx-file-drop-example/src/app/app.component.ts
+++ projects/ngx-file-drop-example/src/app/app.component.ts
@@ -9,8 +9,12 @@ import { NgxFileDropEntry } from 'ngx-file-drop';
 export class AppComponent {
   title = 'ngx-file-drop-example';
 
-  disabled = false;
+  checked = true;
   entries: string[] = [];
+
+  get className(): string {
+    return !this.checked ? 'ngx-file-drop__drop-zone--disabled' : 'ngx-file-drop__drop-zone--enabled';
+  }
 
   dropped(files: NgxFileDropEntry[]): void {
     this.entries = files.map(file => file.relativePath);
