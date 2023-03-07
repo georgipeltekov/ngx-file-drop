cdd7a445315f19448728580f4db2d0185b235859
Bobby Galli <bobbyg603@gmail.com>
2023-03-07 05:25:49 -0500

    Add Example Project

---
+++ projects/ngx-file-drop/src/public-api.ts
@@ -0,0 +1,5 @@
+export { NgxFileDropComponent } from './lib/ngx-file-drop.component';
+export { NgxFileDropModule } from './lib/ngx-file-drop.module';
+export { NgxFileDropEntry } from './lib/ngx-file-drop-entry';
+export { FileSystemEntry, FileSystemDirectoryEntry, FileSystemFileEntry } from './lib/dom.types';
+export { NgxFileDropContentTemplateDirective } from './lib/ngx-templates.directive';
